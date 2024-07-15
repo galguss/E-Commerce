02c41253bdbf11c67bb321d9ebb4e9eed534152f
orzarhi <or.zarhi11@gmail.com>
2024-07-15 18:54:37 +0300

    chore: Refactor adminOnly middleware to handle unauthorized access

--- src/middlewares/adminOnly.js
+++ src/middlewares/adminOnly.js
@@ -1,9 +1,11 @@
 const adminOnly = async (req, res, next) => {
   const user = req.user;
-  
-  if (user.role === "ADMIN") return next();
 
-  return res.status(401).json({ message: "Unauthorized" });
+  if (!user || user.role !== "ADMIN") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  return next();
 };
 
 module.exports = adminOnly;
