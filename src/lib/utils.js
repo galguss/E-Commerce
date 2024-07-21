53b31cd5dff18a529cb0fadcef135c454335204a
orzarhi <or.zarhi11@gmail.com>
2024-07-21 22:56:16 +0300

    chore: Remove unused npm dependency and update CSS styles

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,13 +6,20 @@ const hash = async (value) => bcrypt.hash(value, 10);
 const compareHash = async (value, hash) => bcrypt.compare(value, hash);
 
 const generateToken = async (payload) =>
- await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 
 const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
+
+const formatPrice = (price) =>
+  new Intl.NumberFormat("he-IL", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
 
 module.exports = {
   compareHash,
   hash,
   generateToken,
   verifyToken,
+  formatPrice
 };
