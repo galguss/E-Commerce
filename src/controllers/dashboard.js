741b378bf8cbff4bc0e0c66bea77644b75eebca6
galguss <gal.gus12@gmail.com>
2024-07-26 01:36:31 +0300

    Working with CANVAS and small fixes

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -4,6 +4,58 @@ const User = require("../modules/User");
 const Order = require("../modules/Orders");
 const { verifyToken } = require("../lib/utils");
 
+const salesData = async (req, res) => {
+  try {
+    const orders = await Order.find().populate('productList.product').populate('userId');
+    const products = await Product.find();
+    const accounts = await User.find();
+    
+    
+    const enhancedOrders = orders.map((order) => {
+      const enhancedProductList = order.productList
+        .map((item) => {
+          const product = products.find(
+            (product) => product._id.toString() === item.product.toString()
+          );
+          if (product) {
+            return {
+              ...product._doc,
+              quantity: item.quantity,
+            };
+          }
+          return undefined;
+        })
+        .filter((product) => product !== undefined);
+    
+      const user = accounts.find(user => order.userId.toString() === user._id.toString());
+      const userName = user ? user.fullName : 'Unknown User';
+    
+      return {
+        ...order._doc,
+        userId: userName,
+        productList: enhancedProductList,
+      };
+    });
+
+    const data = enhancedOrders.reduce((acc, order) => {
+      order.productList.forEach((product) => {
+        if (!acc[product.productName]) {
+          acc[product.productName] = 0;
+        }
+        acc[product.productName] += product.price * product.quantity;
+      });
+      return acc;
+    }, {});
+
+    const labels = Object.keys(data);
+    const values = Object.values(data);
+
+    res.status(200).json({ labels, values });
+  } catch (error) {
+    console.error('Error fetching sales data:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
 
 const showDashboard = async (req, res) => {
     const accounts = await User.find();
@@ -57,5 +109,6 @@ const updateProduct = async (req, res) => {
 module.exports = {
     showDashboard,
     addProduct,
-    updateProduct
+    updateProduct,
+    salesData
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const {
     showDashboard,
     addProduct,
-    updateProduct
+    updateProduct,
+    salesData
   } = require("../controllers/dashboard");
 
   router.get('/dashboard', showDashboard);
+  router.get('/sales-data', salesData);
   router.get('/dashboard/add-product', addProduct);
   router.get('/dashboard/update-product/:ID', updateProduct);
 
